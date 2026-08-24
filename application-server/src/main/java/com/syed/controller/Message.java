f3c4b3d438899a82cba33122d4f4858d58482446
Syed Jafar <syed.jafar@singlepointai.com>
2026-08-24 22:36:33 +0530

    refactor: modernize service endpoint

--- application-server/src/main/java/com/syed/controller/Message.java
+++ application-server/src/main/java/com/syed/controller/Message.java
@@ -1,18 +1,20 @@
 package com.syed.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Message {
 
-    @Value("${service.instance.name}")
-    private String instance;
+    private final String instance;
 
-    @RequestMapping("/")
+    public Message(@Value("${service.instance.name:local}") String instance) {
+        this.instance = instance;
+    }
+
+    @GetMapping("/")
     public String message() {
-
         return "Hello from " + instance;
     }
 }
