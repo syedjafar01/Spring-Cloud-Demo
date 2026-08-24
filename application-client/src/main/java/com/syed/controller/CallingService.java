57ff2b8fd11c49ad0db57a4c4561befd536f370f
Syed Jafar <syed.jafar@singlepointai.com>
2026-08-24 23:56:18 +0530

    feat: protect service calls with resilience patterns

--- application-client/src/main/java/com/syed/controller/CallingService.java
+++ application-client/src/main/java/com/syed/controller/CallingService.java
@@ -1,11 +1,16 @@
 package com.syed.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
 @RestController
 public class CallingService {
+
+    private static final String SERVICE_NAME = "service";
+    private static final String FALLBACK_RESPONSE = "Service temporarily unavailable";
 
     private final RestClient restClient;
 
@@ -14,10 +19,16 @@ public class CallingService {
     }
 
     @GetMapping("/")
+    @Retry(name = SERVICE_NAME)
+    @CircuitBreaker(name = SERVICE_NAME, fallbackMethod = "serviceFallback")
     public String callService() {
         return restClient.get()
                 .uri("http://service/")
                 .retrieve()
                 .body(String.class);
+    }
+
+    private String serviceFallback(Throwable throwable) {
+        return FALLBACK_RESPONSE;
     }
 }
