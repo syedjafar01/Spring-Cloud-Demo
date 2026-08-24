1843006c16e9687703057633a5b551ae20130fcc
Syed Jafar <syed.jafar@singlepointai.com>
2026-08-24 23:13:53 +0530

    test: verify service client response handling

--- application-client/src/test/java/com/syed/controller/CallingServiceTest.java
+++ application-client/src/test/java/com/syed/controller/CallingServiceTest.java
@@ -5,15 +5,22 @@ import org.springframework.web.client.RestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Answers.RETURNS_DEEP_STUBS;
 
 class CallingServiceTest {
 
     @Test
-    void shouldCreateClientControllerWithInjectedRestClient() {
-        RestClient restClient = mock(RestClient.class);
+    void shouldReturnResponseFromDiscoveredService() {
+        RestClient restClient = mock(RestClient.class, RETURNS_DEEP_STUBS);
+        when(restClient.get()
+                .uri("http://service/")
+                .retrieve()
+                .body(String.class))
+                .thenReturn("Hello from instance-1");
 
         CallingService service = new CallingService(restClient);
 
-        assertThat(service).isNotNull();
+        assertThat(service.callService()).isEqualTo("Hello from instance-1");
     }
 }
